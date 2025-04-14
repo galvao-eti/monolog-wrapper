c3040f53a5a37613c591a13ca1452bca143cb497
Er Galvão Abbott <galvao@galvao.eti.br>
2025-04-14 03:24:44 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # monolog-wrapper
+
+A wrapper around [monolog](https://github.com/Seldaek/monolog) that implements a few common standards, such as:
+* A single folder path where log files are stored;
+* Using dates (Y-m-d) as a filename;
+* A pattern for log lines that prioritizes severity and time first.
+
+while also letting you add as many streams and processors as you wish.
+
+## Installation
+
+```bash
+composer require galvao-eti/monolog-wrapper
+```
+
+## Usage
+```php
+use GalvaoEti\MonologWrapper;
+
+// If you wish to add streams and/or processors just add them to the static attributes:
+MonologWrapper::$streams['streamName'] = 'streamSeverity';
+MonologWrapper::$processors[] = new \Monolog\Processor\WebProcessor(null, [
+    'ip' => 'REMOTE_ADDR',
+    'referrer' => 'HTTP_REFERER',
+    'method' => 'REQUEST_METHOD',
+]);
+
+$logger = MonologWrapper::getInstance();
+
+// From here onwards just use Moolog's own methods, such as debug(), info(), etc...
+```
+
+## Credits
+Made for [Galvão Desenvolvimento Ltda.](https://galvao.eti.br) by Er Galvão Abbott.
---
+++ src/MonologWrapper.php
@@ -0,0 +1,81 @@
+<?php
+
+/**
+ * GalvaoEti\MonologWrapper
+ * A wrapper around Monolog that implements a few standards and provides a decent level of customization
+ *
+ * @author Er Galvão Abbott <galvao@php.net>
+ * @version 0.1.0
+ * @license https://www.apache.org/licenses/LICENSE-2.0.html Apache 2.0
+ *
+ * @link https://github.com/galvao-eti/monolog-wrapper
+ * @link https://packagist.org/packages/galvao-eti/monolog-wrapper
+ */
+
+declare(strict_types = 1);
+
+namespace GalvaoEti\MonologWrapper;
+
+use \DateTime;
+use \Exception;
+
+use Monolog\{
+    Formatter\LineFormatter,
+    Handler\StreamHandler,
+    Level,
+    Logger,
+};
+
+class MonologWrapper
+{
+    private static ?string $path = null;
+    public static ?Logger $instance = null;
+
+    public static array $streams = [
+        'default' => Level::Debug,
+    ];
+
+    public static array $processors = [];
+
+    public static function getInstance(string $path, string $name): Logger
+    {
+        self::$path = realpath($path);
+
+        if (self::$path === false) {
+            throw new Exception($path . ' is not a valid directory.');
+        }
+
+        if (self::$instance === null) {
+            self::$instance = new Logger($name);
+
+            foreach (self::$processors as $processor) {
+                self::$instance->pushProcessor($processor);
+            }
+
+            array_map(function ($streamName, $streamLevel) {
+                if ($streamName !== 'default') {
+                    self::$path .= '/' . $streamName;
+                }
+
+                if (!is_writable(self::$path)) {
+                    throw new Exception($path . ' is not writable.');
+                }
+
+                $time = new DateTime();
+                $logFile = $time->format('Y-m-d') . '.log';
+
+                $timeFormat = 'H:i:s';
+                $lineFormat = "[ %level_name% ] - %datetime%: %message%\nDATA:\n%extra%\n\n";
+
+                $formatter = new LineFormatter($lineFormat, $timeFormat);
+
+                $stream = new StreamHandler(self::$path . '/' . $logFile, $streamLevel);
+                $stream->setFormatter($formatter);
+
+                self::$instance->pushHandler($stream);
+            }, array_keys(self::$streams), array_values(self::$streams));
+        }
+
+        return self::$instance;
+    }
+}
